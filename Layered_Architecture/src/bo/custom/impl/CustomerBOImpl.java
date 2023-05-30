c61458b2189be6b4babc069aa11ee9a505127848
udarasan <udarassanjeewa@gmail.com>
2023-05-30 09:50:43 +0530

    Bo Factory Implementation and content update

--- Layered_Architecture/src/bo/custom/impl/CustomerBOImpl.java
+++ Layered_Architecture/src/bo/custom/impl/CustomerBOImpl.java
@@ -3,7 +3,6 @@ package bo.custom.impl;
 import bo.custom.CustomerBO;
 import dao.DAOFactory;
 import dao.custom.CustomerDAO;
-import dao.custom.impl.CustomerDAOImpl;
 import model.CustomerDTO;
 
 import java.sql.SQLException;
@@ -12,40 +11,33 @@ import java.util.ArrayList;
 public class CustomerBOImpl implements CustomerBO {
 
     CustomerDAO customerDAO = (CustomerDAO) DAOFactory.getDaoFactory().getDAO(DAOFactory.DAOTypes.CUSTOMER);
-
     @Override
     public ArrayList<CustomerDTO> getAllCustomers() throws SQLException, ClassNotFoundException {
-
         return customerDAO.getAll();
     }
 
     @Override
     public boolean addCustomer(CustomerDTO dto) throws SQLException, ClassNotFoundException {
-
         return customerDAO.add(dto);
     }
 
     @Override
     public boolean updateCustomer(CustomerDTO dto) throws SQLException, ClassNotFoundException {
-
         return customerDAO.update(dto);
     }
 
     @Override
     public boolean existCustomer(String id) throws SQLException, ClassNotFoundException {
-        CustomerDAO customerDAO = new CustomerDAOImpl();
         return customerDAO.exist(id);
     }
 
     @Override
     public boolean deleteCustomer(String id) throws SQLException, ClassNotFoundException {
-        CustomerDAO customerDAO = new CustomerDAOImpl();
         return customerDAO.delete(id);
     }
 
     @Override
     public String generateNewCustomerID() throws SQLException, ClassNotFoundException {
-        CustomerDAO customerDAO = new CustomerDAOImpl();
         return customerDAO.generateNewID();
     }
 }
